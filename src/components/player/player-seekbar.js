49f1848c4791a0cea1075d71e5da86fc04824da5
Arturo Coronel <aoitsu3@gmail.com>
2016-11-05 00:50:08 -0700

    Fixed seekbar time not displaying properly and onClick not throwing the correct porcentage value

--- src/components/player/player-seekbar.js
+++ src/components/player/player-seekbar.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 
 
 type PlayerSeekBarProps = {
@@ -9,33 +9,56 @@ type PlayerSeekBarProps = {
     onClick: Function;
 }
 
-function mapEventToSeekPercentage(cb: Function) {
-    return function(event: any) {
-        const totalWidth = (event.target: HTMLElement).clientWidth;
+
+export class PlayerSeekBar extends Component {
+    seekContainer: ?HTMLElement;
+    refHandler: (ref: HTMLElement) => void;
+    mapEventToOnClickAction: (event: any) => void;
+
+
+    constructor(props: PlayerSeekBarProps) {
+        super(props);
+
+        this.seekContainer = null;
+
+        this.mapEventToOnClickAction = this.mapEventToOnClickAction.bind(this);
+        this.refHandler = this.refHandler.bind(this);
+    }
+
+
+    mapEventToOnClickAction(event: any) {
+        if (!this.seekContainer) return;
+
+        const totalWidth = this.seekContainer.clientWidth;
         const clickPosition = event.pageX - (event.target: HTMLElement).offsetLeft;
         const seekPercentage = (clickPosition / totalWidth) * 100;
+        console.log(totalWidth, clickPosition, seekPercentage);
 
-        cb(seekPercentage);
+        this.props.onClick(seekPercentage);
+    }
+
+    refHandler(ref: HTMLElement) {
+        this.seekContainer = ref;
+    }
+
+
+    render() {
+        const { currentTime='0:00', totalTime='Loading...', progress=0, onClick=()=>{} } = this.props;
+
+        return (
+            <div className="player-seek-bar">
+                <div className="player-seek-bar-total-bar"
+                     ref={this.refHandler} 
+                     onClick={this.mapEventToOnClickAction}>
+                    <div className="player-seek-bar-current-bar" style={{width: `${progress}%`}} />
+                </div>
+
+                <div className="player-seek-bar-time">
+                    <div className="player-seek-bar-time-text"> {currentTime} </div>
+                    <div className="player-seek-bar-time-text"> {totalTime} </div>
+                </div>
+            </div>
+        );
     }
 }
 
-
-export function PlayerSeekBar({ 
-    currentTime='0:00', 
-    totalTime='Loading...', 
-    progress=0,
-    onClick=()=>{}
-}: PlayerSeekBarProps) {
-    return (
-        <div className="player-seek-bar">
-            <div className="player-seek-bar-total-bar" onClick={mapEventToSeekPercentage(onClick)}>
-                <div className="player-seek-bar-current-bar" style={{width: `${progress}%`}} />
-            </div>
-
-            <div className="player-seek-bar-time">
-                <div className="player-seek-bar-time-text"> {currentTime} </div>
-                <div className="player-seek-bar-time-text"> {totalTime} </div>
-            </div>
-        </div>
-    );
-};
