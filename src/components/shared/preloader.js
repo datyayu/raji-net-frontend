a4f1beff7f09cb1468780b881f0d150835ba76f2
Arturo Coronel <aoitsu3@gmail.com>
2016-11-06 20:35:29 -0800

    Moved preloader animations from inline svg to css-based animations

--- src/components/shared/preloader.js
+++ src/components/shared/preloader.js
@@ -6,69 +6,51 @@ export function Preloader() {
         <svg
             width='120px'
             height='120px'
-            xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 100 100"
-            preserveAspectRatio="xMidYMid">
+            preserveAspectRatio="xMidYMid"
+            className="preloader"
+        >
             <rect
                 x="0"
                 y="0"
                 width="100"
                 height="100"
                 fill="none"
+                className="preloader-container"
             />
-            <g>
-                <animate attributeName="opacity" 
-                        dur="2s"
-                        repeatCount="indefinite"
-                        begin="0s"
-                        keyTimes="0;0.33;1"
-                        values="1;1;0" 
-                />
-                <circle
-                    cx="50"
-                    cy="50"
-                    r="40"
-                    stroke="#3498db"
-                    fill="none"
-                    strokeWidth="3"
-                    strokeLinecap="round">
-                    <animate
-                        attributeName="r"
-                        dur="2s"
-                        repeatCount="indefinite"
-                        begin="0s"
-                        keyTimes="0;0.33;1"
-                        values="0;22;44" 
-                    />
-                </circle>
-            </g>
-            
-            <g>
-                <animate
-                    attributeName="opacity"
-                    dur="2s"
-                    repeatCount="indefinite"
-                    begin="-1s"
-                    keyTimes="0;0.33;1"
-                    values="1;1;0"
-                />
-                <circle
-                    cx="50"
-                    cy="50"
-                    r="40"
-                    stroke="#1d5173"
-                    fill="none"
-                    strokeWidth="3"
-                    strokeLinecap="round">
-                    <animate 
-                        attributeName="r"
-                        dur="2s"
-                        repeatCount="indefinite"
-                        begin="1s" 
-                        keyTimes="0;0.33;1"
-                        values="0;22;44"/>
-                </circle>
-            </g>
+
+            <circle
+                cx="50"
+                cy="50"
+                r="40"
+                stroke="#3498db"
+                fill="none"
+                strokeWidth="3"
+                strokeLinecap="round"
+                className="preloader-circle preloader-circle-1"
+            />
+
+            <circle
+                cx="50"
+                cy="50"
+                r="40"
+                stroke="#1d5173"
+                fill="none"
+                strokeWidth="3"
+                strokeLinecap="round" 
+                className="preloader-circle preloader-circle-2"
+            />
+
+            <circle
+                cx="50"
+                cy="50"
+                r="40"
+                stroke="#052f4c"
+                fill="none"
+                strokeWidth="3"
+                strokeLinecap="round" 
+                className="preloader-circle preloader-circle-3"
+            />
         </svg>
     );
 }
